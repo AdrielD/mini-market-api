2013a50c3a599790d4fcf0cadb0779083a7f77fb
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-01-05 18:54:59 -0300

    Finish tests for Categories::Create service

--- spec/services/categories/create_spec.rb
+++ spec/services/categories/create_spec.rb
@@ -5,16 +5,23 @@ RSpec.context 'Services' do
     let(:category_params) { FactoryBot.attributes_for(:category) }
 
     it 'saves a new Category' do
-      expect(Category.count).to eq 0
-      described_class.new(category_params).call
-      expect(Category.count).to eq 1
+      expect{ described_class.new(category_params).call }
+        .to change { Category.count }.by(1)
     end
 
     context 'raises error if' do
       it 'name is empty' do
-        expect{ described_class.new({}).call }.to raise_error(CategoryExceptions::NameIsEmpty)
-        expect{ described_class.new({ name: '' }).call }.to raise_error(CategoryExceptions::NameIsEmpty)
-        expect(Category.count).to eq 0
+        expect{ described_class.new({}).call }
+          .to raise_error(CategoryExceptions::NameIsEmpty)
+          .and change { Category.count }.by(0)
+      end
+
+      it 'category with provided name already exists' do
+        Category.create(category_params)
+
+        expect{ described_class.new(category_params).call }
+          .to raise_error(CategoryExceptions::NameAlreadyExists)
+          .and change { Category.count }.by(0)
       end
     end
   end
