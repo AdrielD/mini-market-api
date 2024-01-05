0002e45bd7dc8fc472abc658c92b983c7aec378e
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-01-05 00:22:45 -0300

    Change ome configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << "ec2-15-229-69-93.sa-east-1.compute.amazonaws.com:3000"
+  config.hosts << "*.compute.amazonaws.com:3000"
 end
