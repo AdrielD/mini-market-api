0e5dd985c6c8287cedd91a22a55fa7deee08b568
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-01-05 00:32:46 -0300

    Solve blocked hosts to be dynamic

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,5 +58,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
-  config.hosts << "*.compute.amazonaws.com:3000"
+  config.hosts << /.*\.compute\.amazonaws\.com/
 end
