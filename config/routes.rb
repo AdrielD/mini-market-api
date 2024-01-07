fc829f4d7150b2b7936464bc056008d589190c57
Adriel Dinelli <adriel.dinelli@gmail.com>
2024-01-07 17:10:31 -0300

    Redirect from root, api and api/v1 to health_check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'api/api#health_check'
+
+  get 'api', to: 'api/api#health_check'
+  get 'api/v1', to: 'api/api#health_check'
+
   namespace :api do
     get 'health_check' => 'api#health_check'
 
@@ -6,5 +11,7 @@ Rails.application.routes.draw do
       resources :products, except: [:new]
       resources :categories, except: [:new]
     end
+
+    get 'api/v1', :to => redirect('/api/health_check')
   end
 end
